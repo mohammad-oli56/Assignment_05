d119f4c69721381a743612227ac76cc3c9a0d2ae
Mohammad oli56 <olimohammad286@gmai.com>
2026-07-29 21:11:23 +0600

    setup shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,10 @@
 
 import "./globals.css";
+import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
+
 
 export default function RootLayout({
   children,
@@ -9,7 +14,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`h-full antialiased`}
+      className={cn("h-full", "antialiased", "font-sans", inter.variable)}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
